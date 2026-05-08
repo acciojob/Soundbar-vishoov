02e85f089cc0e8c90a776682ba27fe8a059254a2
Vishoo Verma <78697370+vishoov@users.noreply.github.com>
2026-05-08 11:28:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 //your JS code here. If required.
 const audio = document.getElementById('audio');
 const buttons = document.querySelectorAll('.btn')
+const stopBtn = document.querySelector('.stop');
+
 
 buttons.forEach(button=>{
 	button.addEventListener('click', ()=>{
 		const sound = button.innerText;
-		audio.src=`sounds/${sound}`;
+		audio.src=`./sounds/${sound}`;
 		audio.play();
 	})
+})
+
+
+stopBtn.addEventListener('click', ()=>{
+	audio.pause();
+	audio.currentTime=0;
+	
 })
