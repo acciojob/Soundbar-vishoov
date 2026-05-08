6152f72142c628506fe20316542b643fc73f9cb8
Vishoo Verma <78697370+vishoov@users.noreply.github.com>
2026-05-08 11:25:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const audio = document.getElementById('audio');
+const buttons = document.querySelectorAll('.btn')
+
+buttons.forEach(button=>{
+	button.addEventListener('click', ()=>{
+		const sound = button.innerText;
+		audio.src=`sounds/${sound}`;
+		audio.play();
+	})
+})
